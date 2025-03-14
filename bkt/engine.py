d79e4cb5100e176f7b0db0d92c6af2b6e20fb9f3
code-wolf-byte <hackerstark37@gmail.com>
2025-03-14 00:17:50 -0700

    Update at Fri, 14 Mar 2025 07:17:50 GMT

---
+++ bkt/engine.py
@@ -0,0 +1,10 @@
+
+class BKTEngine:
+
+    def __init__(self, p_inti = 0.2, p_transit=0.3, p_slip = 0.1, p_guess = 0.1, p_lapse = 0.3):
+        self.p_inti = p_inti
+        self.p_transit = p_transit
+        self.p_slip = p_slip
+        self.p_guess = p_guess
+        self.p_lapse = p_lapse
+
