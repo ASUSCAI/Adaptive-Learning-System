3836c17b9876b23f1695ac92924950c006ceb823
code-wolf-byte <hackerstark37@gmail.com>
2025-03-14 00:45:12 -0700

    Update at Fri, 14 Mar 2025 07:45:12 GMT

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
-
+from database.engine import DatabaseEngine 
 app = Flask(__name__, template_folder='templates')
-
+db = DatabaseEngine('sqlite:///AdaptiveLearning.db')
 
 @app.route('/')
 def index():
